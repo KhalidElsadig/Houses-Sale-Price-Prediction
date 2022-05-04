7cde8805b33c1527bd5fcae2d5f82a0bfa4d8327
KHALID\KHALIDELSADIG <khalidkitok96@gmail.com>
2022-05-04 15:25:18 +0200

    no message

---
+++ README.md
@@ -0,0 +1,42 @@
+# Houses Price Prediction: Project Overview
+
+* Created a tool that predict Houses Price (RMSE = **0.0076**) to help buyers knowing the sell price for the house when buying a house.
+* Investigate more than **70** columns.
+* Engineered features related to buying decision criteria.
+* Optimized Linear, Lasso, Ridge, and Random Forest Regressors, Decision Tree Regressors, Support Vector Machine Regressor and more using GridsearchCV to reach the best model.
+* Built a client facing API using FAST API.
+
+## Code ENV
+
+**Python Version:** 3.9  
+**Packages:** Pandas, Numpy, Sklearn, Matplotlib, Seaborn, FAST API
+
+## Data Cleaning
+
+After reading the data files, I needed to clean it up so that it was usable for our model. I made the following changes and created the following variables:
+
+* Removed all columns with more than **15%** missing values to avoid misleading filling of null values.
+* Normalized the dependent variables using box cox transformation due its good performance in skewed data.
+* Selected features based on its person correlation factor.
+* filled None values with zero.
+* replaced outliers thats are out **3** times SD with mean.
+* scaled numerical columns.
+  
+## EDA
+
+I looked at distribution of independet variables and the dependent.
+
+![alt text](images/EDA.png "EDA")
+
+## Model Building
+
+First, I split the data into train and tests sets with 80% to 20 respectively % with **5** folds for validation choosing **Root Mean Square error** (RMSE) as evaluation metric.
+
+I started with linear regression as baseline then rest of eight models (SVM, Decision Tree, K Nearest Neighbors, Random Forest, AdaBoost Regressor, Gradient Boost Regressor, Ridge Regression and lasso Regression) where Random fores achived the best RMSE score with **0.0088** , **0.0076** in training and testing sets respectively.
+
+Here is all model scores in the training set:
+![alt text](images/scores.png "Models scores")
+
+## Productionization
+
+In this step, I built a FAST API endpoint that was hosted on heroku App website. The API endpoint takes in a request with a list of values from a job listing and returns an estimated price.
---
+++ server/main.py
@@ -0,0 +1,102 @@
+#import Libaries 
+from typing import Optional
+from fastapi import FastAPI, Form,Request
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+import  json 
+from joblib import load
+import numpy as np
+import uvicorn
+from scipy.special import inv_boxcox
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
+## load Model 
+__data_columns = [] 
+__model = []
+def get_predicted_price(num_bath,porch_area,yearremodadd,firstfloorarea,home_quality,Age, garagecars,area,ExterQual,grlivarea):
+    try:
+        loc_index = __data_columns.index()
+    except :
+            loc_index=-1
+    x=np.zeros(len(__data_columns))
+    x[0]= num_bath
+    x[1]= porch_area
+    x[2]= yearremodadd
+    x[3]= firstfloorarea
+    x[4]= home_quality
+    x[5]= Age
+    x[6]= garagecars
+    x[7]= area
+    x[8]= ExterQual
+    x[9]= grlivarea
+
+
+
+    return int(round(inv_boxcox(__model.predict([x])[0],-0.20003295224564416),0))
+
+
+def load_saved_model_data():
+    print('loading model data ')
+    global __data_columns
+    global __model
+    pathcolumns= str(Path(__file__).parent.parent.absolute()/"model/columns.json")
+    with open(pathcolumns,'r') as f :
+        __data_columns = json.load(f)['data_columns']
+        print('columns:',__data_columns)
+    pathmodel= str(Path(__file__).parent.parent.absolute()/"model\Random forest model.pkl")    
+
+    with open (pathmodel,'rb') as f:
+            __model= load(f)
+            print('loading model data is done')
+
+    variables = print(','.join(c for c in (__data_columns) if c not in "'"))
+
+
+# Build web app
+load_saved_model_data()
+app = FastAPI()
+temp = Path(__file__).resolve().parent
+templates = Jinja2Templates(directory=str(Path(temp,"templates")))
+origins = [
+    "http://localhost",
+    "http://localhost:8000"
+   "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.mount(
+    "/server/static",
+    StaticFiles(directory=Path(__file__).parent.parent.absolute() / "server/static"),
+    name="static",
+)
+
+@app.get('/',response_class=HTMLResponse)
+async def home(request:Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post('/predicted_price', response_class=HTMLResponse)
+async def predict_price(request: Request,
+num_bath:float= Form(...),
+porch_area:float= Form(...),
+yearremodadd:float= Form(...),
+firstfloorarea:float= Form(...),
+home_quality:float= Form(...),
+Age:float= Form(...),
+garagecars:float= Form(...),
+area:float= Form(...),
+ExterQual:float= Form(...),
+grlivarea:float= Form(...),
+):
+    response =get_predicted_price(num_bath,porch_area,yearremodadd,firstfloorarea,home_quality,Age, garagecars,area,ExterQual,grlivarea)
+    return templates.TemplateResponse("result.html", {"request": request,"id": response})
+#Run web app
+if __name__ == "__main__":
+    uvicorn.run(app,port=8000,host='localhost')
